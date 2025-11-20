b1acfe9fe3ef22ef1d68ed8bfe7d51fd128ec16f
Isara Cholaseuk <isara@scc-202.scc.bu.edu>
2025-11-19 19:09:14 -0500

    tried training, failed, some cuda error (see log), removed __pycache__

--- metric_depth/dist_train.sh
+++ metric_depth/dist_train.sh
@@ -5,13 +5,13 @@ epoch=120
 bs=4
 gpus=8
 lr=0.000005
-encoder=vitl
-dataset=hypersim # vkitti
+encoder=vitb
+dataset=HyperSim # hypersim vkitti
 img_size=518
 min_depth=0.001
 max_depth=20 # 80 for virtual kitti
 pretrained_from=../checkpoints/depth_anything_v2_${encoder}.pth
-save_path=exp/hypersim # exp/vkitti
+save_path=exp/HyperSim # exp/vkitti
 
 mkdir -p $save_path
 
@@ -23,4 +23,4 @@ python3 -m torch.distributed.launch \
     --master_port=20596 \
     train.py --epoch $epoch --encoder $encoder --bs $bs --lr $lr --save-path $save_path --dataset $dataset \
     --img-size $img_size --min-depth $min_depth --max-depth $max_depth --pretrained-from $pretrained_from \
-    --port 20596 2>&1 | tee -a $save_path/$now.log
+    2>&1 | tee -a $save_path/$now.log
